66e75337fd42fee2ebfc241e378631db0da9c2e5
Pakorn Nathong <goddesign11294_2@hotmail.com>
2022-09-20 00:25:17 +0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+pub mod circuit;
+pub mod random_oracle;
+pub mod signature;
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+
+    use ark_bls12_381::{Fr as BlsFr, G1Affine, G1Projective};
+    use ark_crypto_primitives::encryption::{
+        elgamal::{ElGamal, Randomness},
+        AsymmetricEncryptionScheme,
+    };
+    use ark_ec::AffineCurve;
+    use ark_ff::{BigInteger, PrimeField, UniformRand};
+    use ark_std::test_rng;
+
+    type ElG = ElGamal<G1Projective>;
+
+    #[test]
+    fn test_elgamal_prim() -> Result<(), Box<dyn Error>> {
+        let mut rng = test_rng();
+
+        let param = ElG::setup(&mut rng)?;
+        let (pk, sk) = ElG::keygen(&param, &mut rng)?;
+        let randomness = Randomness::<G1Projective>::rand(&mut rng);
+
+        let two_pnt =
+            G1Affine::from_random_bytes(&BlsFr::from(2).into_repr().to_bytes_be()).unwrap();
+        let two_enc = ElG::encrypt(&param, &pk, &two_pnt, &randomness)?;
+
+        let dec = ElG::decrypt(&param, &sk, &(two_enc.0 + two_enc.0, two_enc.1 + two_enc.1))?
+            .into_projective();
+
+        assert_eq!(two_pnt + two_pnt, dec);
+
+        Ok(())
+    }
+}
