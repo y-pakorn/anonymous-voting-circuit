c9e697c8183c59c66f5aab2da8895a2ff962f1a5
Pakorn Nathong <goddesign11294_2@hotmail.com>
2022-09-20 20:15:06 +0700

    feat: add voting system test

--- src/system/error.rs
+++ src/system/error.rs
@@ -17,3 +17,14 @@ pub enum SystemError {
     #[error("Used Nullifier")]
     UsedNullifier,
 }
+
+impl PartialEq for SystemError {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Ark(l0), Self::Ark(r0)) => l0.to_string() == r0.to_string(),
+            (Self::Synthesis(l0), Self::Synthesis(r0)) => l0 == r0,
+            (Self::Poseidon(l0), Self::Poseidon(r0)) => l0.to_string() == r0.to_string(),
+            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
+        }
+    }
+}
